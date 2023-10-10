24c7fc3f37cee0340494895320882bce9da87f2b
Muhammad Minhajuddin <mminhajuddin@pyrexx.com>
2023-10-11 00:53:37 +0200

    added enum class und comments

---
+++ KeysEnum.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Push\Data;
+enum SecureKeys: string {
+    case PUBLIC_KEY = 'BGcOdUDx04kICiYYEt8ifKGCiq_NuZ7I_9UiogTDpe2WkIOnpdUx8yrJnrHuot9B4f-AOmT39_urJ17fuQ2usWE';
+    case PRIVATE_KEY = 'AsDltFyg5tYfNarSGWomW3_3E20P3CL4cuu0cs2QYpU';
+
+}
